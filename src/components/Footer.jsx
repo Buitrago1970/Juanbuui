0b1609fe073a1924906760194b7ca7352abe9979
Juan Buitrago <juanbuitrago2002@outlook.com>
2022-06-27 13:29:32 -0500

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,70 +1,18 @@
 import * as React from "react";
-
-//styles
-import "./styles/Footer.css";
 //icons
-import {
-  AiFillHeart,
-  AiOutlineInstagram,
-  AiOutlineLinkedin,
-  AiOutlineGithub,
-} from "react-icons/ai";
-//images
-import email from "../images/email.png";
+import { AiFillHeart } from "react-icons/ai";
 
 export default function Work() {
   return (
     <>
-      <footer className="footer" id="footer">
-        <div className="container-rights-reseved">
-          <div className="rights-reseved">
-            <p className="footers">Desing and developed by Juuan Bui.</p>
-          </div>
-          <div className="whit-love">
-            <p className="whit-love-p">
-              Echo Con <AiFillHeart />{' '}
-              en Bogotá, Colombia
-            </p>
-          </div>
-        </div>
-        <div className="email">
-          <div className="social-media">
-            <a
-              className="footer-button instagram"
-              href="https://www.instagram.com/0_0hentay0_0/"
-              target="_blank"
-              rel="noreferrer nofollow"
-            >
-              <AiOutlineInstagram />
-            </a>
-            <a
-              className="footer-button linkedin"
-              href="https://www.linkedin.com/in/juan-sebastian-buitrago-90a769165/"
-              target="_blank"
-              rel="noreferrer nofollow"
-            >
-              {" "}
-              <AiOutlineLinkedin />
-            </a>
-            <a
-              className="footer-button github"
-              href="https://github.com/Buitrago1970"
-              target="_blank"
-              rel="noreferrer nofollow"
-            >
-              <AiOutlineGithub />
-            </a>
-            <a
-              className="footer-button footer-button-email"
-              href="mailto:juanbuitrago2002@outlook.com"
-              title="juanbuitrago2002@outlook.com"
-            >
-              Escríbeme un email
-            </a>
-          </div>
-          <div className="container-img-f">
-            <img className="img-email" src={email} alt="" />
-          </div>
+      <footer id="footer">
+        <p className="footer__principal-text">
+          Desing developed by Juan Buitrago.
+        </p>
+        <div className="whit-love">
+          <p>
+            Echo Con <AiFillHeart /> en Bogotá, Colombia
+          </p>
         </div>
       </footer>
     </>
